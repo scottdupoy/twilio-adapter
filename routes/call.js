2f42ca61872150f736b9bfd24c4fdb21c4814fb6
unknown <gsd@SCOTT.apdev.local>
2013-07-12 12:51:37 +0100

    make calls more dynamic

--- routes/call.js
+++ routes/call.js
@@ -8,10 +8,12 @@ exports.call = function() {
         console.log('  HOST:        ' + request.body.Host);
         console.log('  FROM:        ' + request.body.From);
         console.log('  TO:          ' + request.body.To);
+        console.log('  TYPE:        ' + request.body.Type);
+        console.log('  DATA:        ' + request.body.Data);
         console.log('  ACCOUNT SID: ' + request.body.AccountSid);
         console.log('  AUTH TOKEN:  ' + request.body.AuthToken);
         
-        var callback = request.body.Host + '/twiml/handler/contacted?holy=toast';
+        var callback = request.body.Host + '/twiml/handler/contacted/' + request.body.Type + '/' + request.body.Data;
         console.log('  CALLBACK:    ' + callback);
         
         var twilio = require('twilio')(request.body.AccountSid, request.body.AuthToken);
--- routes/handler.js
+++ routes/handler.js
@@ -1,51 +1,4 @@
 
-exports.incomingCall = function(keys) {
-    return function(request, response) {
-        console.log('INCOMING CALL');
-        response.render('twiml/incoming-initial-response', { brokerConnect: keys.brokerConnect, brokerMessage: keys.brokerMessage});
-    };
-};
-
-exports.handleBrokerChoice = function(keys) {
-    return function(request, response) {
-        if (request.body.Digits && request.body.Digits == keys.brokerConnect) {
-            console.log('BROKER CHOICE: CONNECT TO HANDLER');
-            response.render('twiml/connecting-to-handler');
-        }
-        else if (request.body.Digits && request.body.Digits == keys.brokerMessage) {
-            console.log('BROKER CHOICE: LEAVING A MESSAGE');
-            response.render('twiml/leave-a-message');
-        }
-        else {
-            console.log('BROKER CHOICE: INVALID DIGIT');
-            response.render('twiml/hang-up', { message: 'Invalid option, good bye.' });
-        }        
-    }
-};
-
-exports.statusCallback = function() {
-    return function(request, response) {
-        console.log('STATUS CALLBACK');
-        response.end();
-    }
-};
-
-exports.handleBrokerMessage = function(keys) {
-    return function(request, response) {        
-        console.log('BROKER MESSAGE: MESSAGE DURATION = ' + request.body.RecordingDuration + ' SECONDS - TODO: IGNORE IF TO SHORT');
-        if (request.body.CallStatus && request.body.CallStatus == "in-progress") {
-            // either timedout or broker pressed a key to complete message.  need to hang-up either way.
-            console.log('BROKER MESSAGE: BROKER STILL ON LINE - HANGING UP');
-            response.render('twiml/hang-up', { message: 'Your message will be forwarded to a handler. Good bye.' });
-        }
-        else {
-            // broker hung-up to complete message
-            console.log('BROKER MESSAGE: BROKER HUNG UP TO COMPLETE MESSAGE');
-            response.end();
-        }
-    };
-};
-
 exports.handleHandlerContacted = function(keys) {
     return function(request, response) {    
         console.log('HANDLER CONTACTED');        
@@ -86,17 +39,3 @@ exports.handleHandlerContacted = function(keys) {
         }        
     };
 };
-/*
-<?xml version='1.0' encoding='UTF-8'?>
-<Response>                                                                                                
-    <Gather action='/twiml/handler/accept-result/<%= type %>/<%= data %>' timeout='12' method='POST' numDigits='1'>                             
-        <Say>                                                                                             
-            <%= message => Please press <%= handlerAccept %> to accept, <%= handlerReject %> to reject or hang up if you do not work for Altis.
-        </Say>                                                                                            
-    </Gather>
-    <Say>
-        No response detected, good bye.
-    </Say>
-    <Hangup/>
-</Response>
-*/
